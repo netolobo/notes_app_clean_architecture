c527bc9e8a592e03c2adaf81425ad9600e3c4bf2
netolobo <desilio@gmail.com>
2023-06-10 16:17:28 -0700

    build.gradle updated from Groovy to Kotlin kts.

---
+++ app/build.gradle.kts
@@ -0,0 +1,111 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.netoloboapps.noteapp"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.netoloboapps.noteapp"
+        minSdk = 29
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "com.netoloboapps.noteapp.HiltTestRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.7"
+    }
+    packaging {
+        resources {
+            resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+    hilt {
+        enableAggregatingTask = true
+    }
+}
+
+dependencies {
+    //Navigation components
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+
+    //ViewModel-Compose
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    //Room
+    kapt ("androidx.room:room-compiler:2.5.1")
+    implementation ("androidx.room:room-ktx:2.5.1")
+
+    //Hilt
+    implementation ("com.google.dagger:hilt-android:2.45")
+    kapt ("com.google.dagger:hilt-compiler:2.45")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation ("com.google.dagger:dagger:2.45")
+    kapt ("com.google.dagger:dagger-compiler:2.45")
+
+    //Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    //Material icons extended
+    implementation ("androidx.compose.material:material-icons-extended:1.4.3")
+
+    implementation ("androidx.core:core-ktx:1.10.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation (platform("androidx.compose:compose-bom:2023.04.01"))
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material3:material3:1.1.0")
+
+    // Local unit tests
+    testImplementation ("androidx.test:core:1.5.0")
+    testImplementation ("junit:junit:4.13.2")
+    testImplementation ("androidx.arch.core:core-testing:2.2.0")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation ("com.google.truth:truth:1.1.3")
+    testImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
+    testImplementation ("io.mockk:mockk:1.10.5")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest")
+
+    //Instrumentation tests
+    androidTestImplementation ("com.google.dagger:hilt-android-testing:2.43.2")
+    kaptAndroidTest ("com.google.dagger:hilt-android-compiler:2.44.2")
+    androidTestImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation ("com.google.truth:truth:1.1.3")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test:core-ktx:1.5.0")
+    androidTestImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
+    androidTestImplementation ("io.mockk:mockk-android:1.10.5")
+    androidTestImplementation ("androidx.test:runner:1.5.2")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+}
